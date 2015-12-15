d4a13a36fb8e6c5136bd94d5ee8395d9196a4d50
Lawrence Liu <lawrence255@yahoo.com>
2015-12-15 18:46:26 -0500

    Created Temperature class and added converter methods.

---
+++ Temperature.java
@@ -0,0 +1,11 @@
+public class Temperature{
+
+    public static double CtoF (double t){
+	return (t*1.8)+32.0;
+    }
+
+    public static double FtoC (double t){
+	return (t-32.0)*5.0/9.0;
+    }
+
+}
